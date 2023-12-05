f2e96c19ea31fa9c46aeec807485aa481b7b9de5
Thái Ngọc Rạng <98004370+restom0@users.noreply.github.com>
2023-12-05 22:03:25 +0700

    hello

--- server/src/config/db/index.js
+++ server/src/config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/ssps');
+    await mongoose.connect('mongodb+srv://thaingocrang2014:wh3rhS2eaeh5rQNK@cluster0.lcj2oyv.mongodb.net/ssps');
     console.log('Connect success');
   } catch (error) {
     console.log('Connect failed');
